a2dcadc01bcb77f05cd0cb0f72b4857fa07808c6
ndhieu2701 <hieu@v7rx3.onmicrosoft.com>
2023-04-21 09:13:07 +0700

    first commit

---
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.test;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView mNavigationView;
+    private ViewPager mViewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNavigationView = findViewById(R.id.bottom_nav);
+        mViewPager = findViewById(R.id.view_pager);
+
+        setUpViewPager();
+
+        mNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        mViewPager.setCurrentItem(0);
+                        break;
+                    case R.id.action_favorite:
+                       mViewPager.setCurrentItem(1);
+                        break;
+                    case R.id.action_my_page:
+                        mViewPager.setCurrentItem(2);
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    private void setUpViewPager(){
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        mViewPager.setAdapter(viewPagerAdapter);
+    }
+}
